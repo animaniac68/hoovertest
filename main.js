ab634f5697a3b507e33e1f9e432e9b30c05225e4
animaniac68 <ani.shajar68@gmail.com>
2019-12-22 03:14:03 +0000

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var hoover = {
 
 console.log ('Your current position is: + hoover.position');
 
-//his is the room in a grid rectangle where the hoover will do it's 
+//This is the room as a grid rectangle where the hoover will do it's
 //life's sole purpose (to clean an imaginary room)
 
 function createGrid(columns,rows) {
@@ -21,3 +21,32 @@ function createGrid(columns,rows) {
 //the dirty room has 5 columns and 5 rows.
 var dirtyRoom = createGrid (5,5);
 console.log (dirtyRoom);
+
+//Now we need to make the hoover move by creating some controllers,
+//the controllers will be cardinal (Naughty Elephants Squirt Water) ;-p
+//(I mean North East South West)
+
+function commands(command){
+ for var i = 0; i < command.length; i++)
+ {
+switch (command [i]){
+
+  case 'N':
+  moveNorth();
+  break;
+
+  case 'E':
+  moveEast();
+  break;
+
+  case 'S';
+  moveSouth();
+  break;
+
+  case 'W';
+  moveWest;
+  break;
+}
+   console.log (hoover)
+ }
+}
