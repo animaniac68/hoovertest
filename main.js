38149cabc0784775922771385a1666c578eb0e77
animaniac68 <ani.shajar68@gmail.com>
2020-01-12 21:12:19 +0000

    code clean up

--- main.js
+++ main.js
@@ -1,51 +1,27 @@
 //This code allows the input.txt file to be read, each line is an element of the array readInput
 var fs = require('fs');
 var readInput = fs.readFileSync('input.txt', 'utf8').toString().split("\n");
-console.log('INPUT DATA');
-console.log(readInput);
-console.log('=================');
-
-//read GRID dimensions
-
+//**TEST console.log('INPUT DATA');
+//**TEST console.log(readInput);
+//Read GRID dimensions
 var gridSize = readInput[0].split(/[ , ]+/);
-console.log ('The input grid size is: ' + gridSize);
-
-
-console.log('=====================');
-//read HOOVER position
+//**TEST console.log ('The input grid size is: ' + gridSize);
+//Read HOOVER position
 var hoover = {
-  //position:[0, 2]}
   position:readInput[1].split(/[ ,]+/).map(Number)};
-
-
-console.log ('The hoover is at start position: ' + hoover.position);
-
-console.log('=====================');
-
-//read DIRT position
+//**TEST console.log ('The hoover is at start position: ' + hoover.position);
+//Read DIRT position
 var dirt = {
   a: readInput[2].split(/[ , ]+/),
   b: readInput[3].split(/[ , ]+/),
   c: readInput[4].split(/[ , ]+/)
 };
-//var dirt1 = readInput[2].split(/[ ,]+/);
-//var dirt2 = readInput[3].split(/[ ,]+/);
-//var dirt3 = readInput[4].split(/[ ,]+/);
-
-
-console.log ('The dirt is at positions: ' + dirt.a +' , ' + dirt.b +' , '+ dirt.c);
-//=================================
-console.log('=====================');
-console.log('CARDINAL INPUT:');
+//**TEST console.log ('The dirt is at positions: ' + dirt.a +' , ' + dirt.b +' , '+ dirt.c);
 //Cardinal Directions read from input
 var cardinalInput = readInput[5];
-console.log (cardinalInput);
+//**TEST console.log (cardinalInput);
 
-console.log('=======================');
-
-//==============================
-console.log('GRID:');
-
+//Now lets create the dirty room (grid) for our hoover to roam around in and clean up.
 // CREATE Grid
 function createGrid(columns, rows) {
 	var grid = [];
@@ -55,246 +31,96 @@ function createGrid(columns, rows) {
 	return grid;
 };
 
-//TEST creating grid with elements in a similar format to input data
-
 var myGrid = createGrid(5,5);
-// ['1 5','2 5','3 5','4 5','5 5'],
-// ['1 4','2 4','3 4','4 4', '5 4'],
-// ['1 3','2 3','3 3','4 3', '5 3'],
-// ['1 2','2 2','3 2','4 2', '5 2'],
-// ['1 1','2 1','3 3','4 1', '5 1']];
-//createGrid(5, 5);
+//**TEST console.log(myGrid);
 
-console.log(myGrid);
-console.log('=====================');
+//This small piece of code logs the travel movements of the hoover.
 var travelLog = [];
-//  lets  find the location of the Hoover index on myGrid, as the input data is a string that cannot be passed as int due to a blank space in between, we will use the hoover index to navigate around the grid and then call the element string value when the hoover moves.
-//
-// function getIndexOfH(myGrid, hoover) {
-//   for (var i = 0; i < myGrid.length; i++) {
-//     var index = myGrid[i].indexOf(hoover);
-//     if (index > -1) {
-//       return [i, index];
-//     }
-//   }
-// }
-// //search the grid with the string input e.g. '1 3' location, then find the corresponding grid index.
-// var hooverI = getIndexOfH(myGrid, hoover.position);
-// //test get index of hoover
-// console.log('The hoover ' + hoover.position + ' is located in myGrid array at ' + hooverI + '.' );
 
-//now do the same for the dirt
-// Find the location of the Dirt index on my grid
-//=========Dirt1
-// function getIndexOfD1(myGrid, dirt1) {
-//   for (var i = 0; i < myGrid.length; i++) {
-//     var index = myGrid[i].indexOf(dirt1);
-//     if (index > -1) {
-//       return [i, index];
-//     }
-//   }
-// }
-
-// var dirt1L = getIndexOfD1(myGrid, dirt1);
-// //test get index of dirt1
-// console.log('The dirt ' + dirt1 + ' is located in myGrid array at ' + dirt1L + '.' );
-// //==========Dirt 2
-// function getIndexOfD2(myGrid, dirt2) {
-//   for (var i = 0; i < myGrid.length; i++) {
-//     var index = myGrid[i].indexOf(dirt2);
-//     if (index > -1) {
-//       return [i, index];
-//     }
-//   }
-// }
-// var dirt2L = getIndexOfD2(myGrid, dirt2);
-// //test get index of dirt2
-// console.log('The dirt ' + dirt2 + ' is located in myGrid array at ' + dirt2L + '.' );
-// //=========Dirt 3
-// function getIndexOfD3(myGrid, dirt3) {
-//   for (var i = 0; i < myGrid.length; i++) {
-//     var index = myGrid[i].indexOf(dirt3);
-//     if (index > -1) {
-//       return [i, index];
-//     }
-//   }
-// }
-
-// var dirt3L = getIndexOfD3(myGrid, dirt3);
-// //test get index of dirt3
-// console.log('The dirt ' + dirt3 + ' is located in myGrid array at ' + dirt3L + '.' );
-
-
-//=======================
-
-console.log('=====================');
-
-//movement of Hoover
-
-//moveNorth
-// function moveNorth() {
-// 	console.log('moveNorth was called');
-// 	switch (dirtbuster) {
-// 		case 'N':
-//     	hoover.position[1] = hoover.position[1] + 1;
-// 			break;
-// 			}
-//       //Stop();
-//       RegisterTravelLogMovement();
-
-//       console.log(hoover)
-// };
-// function moveSouth() {
-// 	console.log('moveSouth was called');
-
-// 	switch (dirtbuster) {
-// 		case 'S':
-// 			hoover.position[1] = hoover.position [1]-1;
-// 			break;
-//       }
-// //Stop();
-
-//       RegisterTravelLogMovement();
-
-//       console.log(hoover)
-// };
-// function moveEast() {
-// 	console.log('moveEast was called');
-
-// 	switch (dirtbuster) {
-// 		case 'E':
-// 			hoover.position[0] = hoover.position[0]
-//       + 1;
-// 			break;}
-//       //Stop();
-//        RegisterTravelLogMovement();
-
-//       console.log(hoover)
-// };
-// function moveWest() {
-// 	console.log('moveWest was called');
-
-// 	switch (dirtbuster) {
-// 		case 'W':
-// 			hoover.position[0] = hoover.position[0]
-//       - 1;
-// 			break;}
-//       //Stop();
-//        RegisterTravelLogMovement();
-//       console.log(hoover)
-// };
-
-
+//************** MOVEMENT
+//This code ensure that the hoover does not move out of the boundaries of the grid.
+//If the hoover moves outside the boundary, the position is reset to previous position.
 function Stop(movement) {
 	if (hoover.position[0] < 0 || hoover.position[0] >= 10) {
-		console.log('Stop! your reached the border, you cannot move ' + movement);
+		//**TEST console.log('Stop! your reached the border, you cannot move ' + movement);
 		hoover.position[0] = 0;
 	}
 	if (hoover.position[1] < 0) {
-		console.log('Stop! your reached the border, you cannot move ' + movement);
+		//**TEST console.log('Stop! your reached the border, you cannot move ' + movement);
 		hoover.position[1] = 0;
 	} else {
 		RegisterTravelLogMovement();
 	}
-}
+};
+
+//This code moves the hoover around the grid
 function commands(command) {
 	for (var i = 0; i < command.length; i++) {
 		switch (command[i]) {
 //MOVE NORTH
       case 'N':
       hoover.position[0] = hoover.position[0] + 1;
-    console.log('moveNorth was called');
+//**TEST    console.log('moveNorth was called');
 RegisterTravelLogMovement();
-//Dirtbuster();
 			break;
-				//moveNorth();
+
 //MOVE SOUTH
 			case 'S':
       	hoover.position[0] = hoover.position [0]-1;
-        console.log('movesSouth was called');
+        //**TEST console.log('movesSouth was called');
         RegisterTravelLogMovement();
-        //Dirtbuster();
-
-				//moveSouth();
 				break;
+
 //MOVE EAST
         case 'E':
         hoover.position[1] = hoover.position[1]
       + 1;
-      console.log('moveEast was called');
+//**TEST      console.log('moveEast was called');
         RegisterTravelLogMovement();
-        //Dirtbuster();
-
-				//moveEast();
 				break;
+
 //MOVE WEST
         case 'W':
         	hoover.position[1] = hoover.position[1]
       - 1;
-      console.log('moveWest was called');
+//**TEST      console.log('moveWest was called');
        RegisterTravelLogMovement();
-       //Dirtbuster();
-
-				//moveWest();
 				break;
     }
       Stop ();
-
-      console.log(hoover)
+      //**TEST console.log(hoover)
   }
 };
-console.log('=====================');
 
-
+//This code gets the new position of the hoover
 function RegisterTravelLogMovement() {
-	travelLog.push(
-		'Hoover moved, Hoovers position is: ' + hoover.position
-	);
-  //isThereDirtHere();
+	travelLog.push(hoover.position);
  Dirtbuster();
 };
 
+//************* DIRT
+//This code counts the number of dirt patches that were cleaned by the hoover
 var counter;
 counter = 0;
-//counter = ++counter;
 
 function Dirtbuster () {
 if (hoover.position[0] == dirt.a[0] && hoover.position[1]== dirt.a[1]){
 counter = counter + 1;
-//delete myGrid[dirt];
-//counter = counter + 1;
-console.log('dirt busted! Total busted: '+ counter);
-}}
-//console.log('dirt busted! Total busted: '+ counter)
-
-
-
-//Commands Testing
-console.log('COMMANDS TESTING ON GRID');
-console.log('=================');
+//**TEST console.log('dirt busted! Total busted: '+ counter);
+};
+};
 
 commands(cardinalInput);
+//**TEST console.log(travelLog);
 
-console.log('=================');
-//====================
-console.log('TRAVEL LOG');
-
-console.log(travelLog);
-console.log('=================');
-
-console.log('=================');
-
-//========================
-console.log('COMMANDS TESTING ON GRID');
-
-console.log('final hoover position: ' + hoover.position);
-console.log('dirt busted: '+ counter);
-
-//place all objects on the grid
-myGrid[hoover.position[0]][hoover.position[1]] = 'H';
-myGrid[dirt.a[0]][dirt.a[1]] = 'D';
-myGrid[dirt.b[0]][dirt.b[1]] = 'D';
-myGrid[dirt.c[0]][dirt.c[1]] = 'D';
+//=======================================
+//******** OUTPUTS **********************
+// **** FINAL HOOVER POSITION *****
+console.log(hoover.position);
+//**** TOTAL NUMBER OF DIRT CLEANED ****
+console.log(counter);
+//=======================================
 
 
-console.log(myGrid);
+
+//**TEST console.log(myGrid);
